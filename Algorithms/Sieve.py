33e748534439233d3f6b19ab1eb93a62c735e608
Galen Scovell <galen.scovell@gmail.com>
2015-01-28 19:31:51 -0800

    Add Sieve of Eratosthenes

---
+++ Algorithms/Sieve.py
@@ -0,0 +1,19 @@
+
+from sys import argv
+
+
+
+def sieve(n):
+    primes = []
+    sieve = [True] * (n + 1)
+    for p in range(2, n + 1):
+        if sieve[p]:
+            primes.append(p)
+            for i in range(p * p, n + 1, p):
+                sieve[i] = False
+    return primes
+
+
+
+if __name__ == '__main__':
+    print(sieve(int(argv[1])))
